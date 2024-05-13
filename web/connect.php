43a1d10fe7887a496fab9baa0c753917c988e97d
lacina_T <100506057+lacinatoure0730@users.noreply.github.com>
2024-05-13 15:47:02 +0000

    Update connect.php

--- web/connect.php
+++ web/connect.php
@@ -1,7 +1,7 @@
 <?php
 
 // Connect to the database
-$servername = "targetdatabase.c1ou2gs0ctlv.eu-north-1.rds.amazonaws.com";
+$host = "targetdatabase.c1ou2gs0ctlv.eu-north-1.rds.amazonaws.com";
 $username = "admin";
 $password = "123456789";
 $dbname = "test";
